dbcc30f5f62b9332b37075288c2646c93dc36aea
Treasure Alekhojie <treasurealekhojie@yahoo.com>
2024-06-20 13:11:07 +0100

    [fix]: Correct API connection URL to resolve connectivity issue [fix]: Resolve routing issue caused by incorrect deployment URL
    Signed-off-by: Treasure Alekhojie <treasurealekhojie@yahoo.com>

--- client/utils/url-utils.ts
+++ client/utils/url-utils.ts
@@ -3,12 +3,12 @@ export function getApiUrl(path: string) {
     return `http://localhost:8080/${path}`;
   }
 
-  return `https://api.open-company.com/${path}`;
+  return `https://api-thingcast.vercel.app/${path}`; // replace with your API URL
 }
 export function getAppUrl(path: string) {
   if (process.env.NODE_ENV === "development") {
     return `http://localhost:3000/${path}`;
   }
 
-  return `https://app.open-company.com/${path}`;
+  return `https://thingcast.vercel.app/${path}`; //NOTE: leave this as is
 }
